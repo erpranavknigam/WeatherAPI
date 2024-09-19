6a7bc5ff00f0fb7edf3392f3205b9f4785d0339a
= <=>
2024-09-19 22:28:01 +0530

    Added Speed Limiter

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const routes = require('./routes/weatherRoutes')
-dotenv.config()
+const express = require('express');
+const slowDown = require('express-slow-down');
+const dotenv = require('dotenv');
+const routes = require('./routes/weatherRoutes');
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use('/api/v1', routes)
+const speedLimiter = slowDown({
+    windowMs: 15 * 60 * 1000,
+    delayAfter: 100,
+    delayMs: () => 500,
+    outLimitReached: (req,res) => {
+        res.status(409).json({messgae:"You have exceeded the maximum number of request, please try again after some time."})
+    }
+});
 
+app.use('/api/v1', speedLimiter, routes);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => `Server running on port ${port} 🔥`);
-
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
